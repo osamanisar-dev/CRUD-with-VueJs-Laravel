78f6a24f5cc54ee96fe7391d91004a9ec663df93
osama <osamanisar682@gmail.com>
2025-05-11 19:15:35 +0500

    Include vue-proj as a regular folder in the main repo

---
+++ resources/js/vue-proj/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <NavbarComponent/>
+    <router-view/>
+</template>
+
+<script>
+import NavbarComponent from "@/components/NavbarComponent.vue";
+export default {
+  name: 'App',
+  components: {NavbarComponent}
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/vue-proj/src/components/HomeComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="p-5 mb-4 bg-light rounded-3 shadow-sm">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Welcome to Our Site</h1>
+                <p class="col-md-8 fs-4">
+                    This is a simple homepage built using Vue 3 and Bootstrap 5. You can customize this layout for your needs.
+                </p>
+                <button class="btn btn-primary btn-lg" type="button">Get Started</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeComponent',
+}
+</script>
+
+<style>
+.text-purple {
+    color: rebeccapurple;
+}
+</style>
